6d402bf5e834512972e24f3b754ade24ef55ccf0
Bulacia <beueleceia@gmail.com>
2024-10-26 10:58:12 -0400

    despliegue prueba en vercel

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">About Page</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">Landing Page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
