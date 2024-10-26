166249b14aab9c982c004c94cd2030b6036e2dae
Bulacia <beueleceia@gmail.com>
2024-10-26 11:03:50 -0400

    link para about en landing

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function LandingPage() {
   return (
     <div>
       <h1 className="text-3xl underline">Landing Page</h1>
+      <Link to="/about">About</Link>
     </div>
   );
 }
